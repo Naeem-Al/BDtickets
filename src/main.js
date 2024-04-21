08c4cfa5b1035a2cacabbe9d8a1bf7dcf307e69f
Naeem-Al <naeemali58013@gmail.com>
2024-04-21 14:51:58 +0600

    add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-function naeem(){
-    document.getElementById("nipu").innerHTML=date()
-}
-naeem()
+const showLoaderButton = document.getElementById('showLoaderButton');
+const loader =document.getElementById('loader');
+
+showLoaderButton.addEventListener('click',function(){
+    loader.style.display ='block';
+
+    setTimeout(function(){
+        loader.style.display='none';
+    },3000);
+});
